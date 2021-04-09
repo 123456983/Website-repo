b50910b955995f88164fe95e2121b0529b1d1708
nivedita <65904972+123456983@users.noreply.github.com>
2021-04-09 17:00:28 +0530

    update

--- Script/install_dependencies.sh
+++ Script/install_dependencies.sh
@@ -1,4 +1,7 @@
 
 #!/bin/bash
-sudo amazon-linux-extras install php7.4
-sudo yum install -y httpd mariadb-server php
+sudo yum update -y
+sudo yum install wget -y
+wget https://aws-codedeploy-us-east-2.s3.us-east-2.amazonaws.com/latest/install
+chmod +x ./install
+sudo ./install auto

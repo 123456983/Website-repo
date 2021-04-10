3204fd18f92dc50ba6362ba3380e2e7698d15ab8
nivedita <65904972+123456983@users.noreply.github.com>
2021-04-10 13:30:16 +0530

    update

--- Script/install_dependencies.sh
+++ Script/install_dependencies.sh
@@ -1,7 +1,3 @@
 
 #!/bin/bash
-sudo yum update -y
-sudo yum install wget -y
-wget https://aws-codedeploy-us-east-2.s3.us-east-2.amazonaws.com/latest/install
-chmod +x ./install
-sudo ./install auto
+yum install -y httpd
